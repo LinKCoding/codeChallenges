9c0d3648ebaeb388fa9f4128bfe1913a838e4e0c
linkcoding <kenny.lin.91@gmail.com>
2020-01-25 20:34:11 -0500

    added JS solutions

---
+++ PeakIndexInAMountainArray/solution.js
@@ -0,0 +1,27 @@
+/* Thoughts
+Since it's always going to be a "mountain"
+And there's only one peak, which will always be greater than the element before and after
+return the index as soon as it's found
+(though can a peak exist at the end?)
+*/
+
+const peakIndexInMountainArray = A => {
+  for(let i = 1; i < A.length; i++) {
+    if(A[i] > A[i - 1] && A[i] > A[i + 1]){
+      return i
+    }
+  }
+};
+
+console.log(peakIndexInMountainArray([0, 1, 2, 1, 0]))
+
+// Example 1:
+
+// Input: [0, 1, 0]
+// Output: 1
+
+// Example 2:
+
+// Input: [0, 2, 1, 0]
+// Output: 1
+
